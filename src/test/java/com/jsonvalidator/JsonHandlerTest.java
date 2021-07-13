afcc266737bb2167ae07dde69907fb202208dc46
Antonio Montero <monterodorado@gmail.com>
2021-07-13 10:00:00 +0200

    Add new test

--- src/test/java/com/jsonvalidator/JsonHandlerTest.java
+++ src/test/java/com/jsonvalidator/JsonHandlerTest.java
@@ -13,27 +13,35 @@ import org.everit.json.schema.loader.SchemaLoader;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class JsonHandlerTest {
 
     User us = null;
     String json;
-    // JsonSchema userSchema;
+    
+    @Rule
+    public ExpectedException exceptionRule = ExpectedException.none();
 
     @Before
     public void setUp() {
         json = "{\"address\": {\"country\": \"Spain\",\"city\": \"Madrid\",\"street\": \"Calle Alcala\"},\"gender\": \"Male\",\"name\": \"John\",\"id\": 1,\"email\": \"john@gmail.com\"}";
+    }
+
+
+    private void generateSchema() {
         // HyperSchemaFactoryWrapper userVisitor = new HyperSchemaFactoryWrapper();
         // ObjectMapper mapper = new ObjectMapper();
         // try {
-        // mapper.acceptJsonFormatVisitor(User.class, userVisitor);
-        // userSchema = userVisitor.finalSchema();
-        // String schemaString =
-        // mapper.writerWithDefaultPrettyPrinter().writeValueAsString(userSchema);
-        // System.out.println(schemaString);
+        //     mapper.acceptJsonFormatVisitor(User.class, userVisitor);
+        //     userSchema = userVisitor.finalSchema();
+        //     String schemaString =
+        //     mapper.writerWithDefaultPrettyPrinter().writeValueAsString(userSchema);
+        //     System.out.println(schemaString);
         // } catch (Exception e) {
-        // userSchema = null;
+        //     userSchema = null;
         // }
     }
 
@@ -58,41 +66,75 @@ public class JsonHandlerTest {
     }
 
     @Test
-    public void givenCreateJsonUserWithoutNameThenSchemaValidationFails() {
-        System.out.println(json);
+    public void givenUserWithoutName_whenValidateJson_ThenSchemaValidationFails() {
+
+        System.out.println("\t-" + new Object(){}.getClass().getEnclosingMethod().getName() + "()");
+        // THEN
+        exceptionRule.expect(ValidationException.class);
+        exceptionRule.expectMessage("required key [name] not found");
+
+        // GIVEN
         String validJson = JsonPath.parse(json).delete("$..['name']").jsonString();
         System.out.println(validJson);
         
         assertTrue("Json is not valid", isJsonValid(validJson));
         try {
+            // WHEN
             Schema schema = getSchema("/schema.json");
             schema.validate(new JSONObject(validJson)); // throws a ValidationException if this object is invalid
         } catch (IOException e) {
             assertTrue("Error loading Schema", false);
             return;
-        } catch (ValidationException ve) {
-            assertTrue("Schema validation fails because of \"name\" field", true);
-            return;
-        }
-        assertTrue("Name field is mandatory!", false);
+        } 
+        
     }
 
     @Test
-    public void givenCreateJsonUserWithoutAddressThenSchemaValidationWorks() {
-        System.out.println(json);
+    public void givenUserWithoutAddress_whenValidateJson_ThenSchemaValidationWorks() {
+
+        System.out.println("\t-" + new Object(){}.getClass().getEnclosingMethod().getName() + "()");
+
+        //GIVEN
         String validJson = JsonPath.parse(json).delete("$..['address']").jsonString();
         System.out.println(validJson);
         
         assertTrue("Json is not valid", isJsonValid(validJson));
         try {
+            //WHEN
             Schema schema = getSchema("/schema.json");
             schema.validate(new JSONObject(validJson)); // throws a ValidationException if this object is invalid
         } catch (IOException e) {
-            System.out.print("Error loading Schema");
+            assertTrue("Error loading Schema", false);
+            return;
+        //THEN
         } catch (ValidationException ve) {
             assertTrue("Schema validation fails because of \"address\" field is optional", false);
         }
         assertTrue("address field is optional. OK", true);
+    }
+
+    @Test
+    public void givenUserWrongTypeOfId_whenValidateJson_ThenSchemaValidationFails() {
+
+        System.out.println("\t-" + new Object(){}.getClass().getEnclosingMethod().getName() + "()");
+        //THEN
+        exceptionRule.expect(ValidationException.class);
+        exceptionRule.expectMessage("id: expected type: Number, found: String");
+
+        //GIVEN
+        String validJson = new JSONObject(json).put("id","algo").toString();
+        System.out.println(validJson);
+        
+        assertTrue("Json is not valid", isJsonValid(validJson));
+        try {
+            //WHEN
+            Schema schema = getSchema("/schema.json");
+            schema.validate(new JSONObject(validJson)); // throws a ValidationException if this object is invalid
+        } catch (IOException e) {
+            assertTrue("Error loading Schema", false);
+            return;
+        } 
+        
     }
 
 }
